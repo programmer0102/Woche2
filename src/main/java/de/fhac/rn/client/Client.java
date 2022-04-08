7e79194f49be7c217f05f0467f7e97fd95342aba
Jonas Hassenrik <jonas.hassenrik@alumni.fh-aachen.de>
2022-04-08 09:18:28 +0200

    Teilaufgabe läuft

--- src/main/java/de/fhac/rn/client/Client.java
+++ src/main/java/de/fhac/rn/client/Client.java
@@ -12,11 +12,17 @@ public class Client {
         var outToServer = new DataOutputStream(clientSocket.getOutputStream());
         var inFromServer = new DataInputStream(clientSocket.getInputStream());
 
-        var message = scanner.nextLine();
-        outToServer.writeUTF(message);
-        var echoMessage = inFromServer.readUTF();
-        System.out.println(echoMessage);
 
+        boolean containsExit = false;
+        while (!containsExit) {
+            var message = scanner.nextLine();
+            outToServer.writeUTF(message);
+            var echoMessage = inFromServer.readUTF();
+            System.out.println(echoMessage);
+            if(echoMessage.contains("/exit")){
+                containsExit=true;
+            }
+        }
         scanner.close();
         clientSocket.close();
     }
